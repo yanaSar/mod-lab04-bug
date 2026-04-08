24f1019b4c6eed096f533daf3f10d98b1552f548
yanaSar <Sarajkina.Yana@yandex.ru>
2026-04-08 15:21:16 +0300

    first

---
+++ bugpro/Program.cs
@@ -0,0 +1,97 @@
+﻿using Stateless;
+namespace BugPro
+{
+
+public class Bug 
+{
+    public enum State 
+    {
+        NewDefected,
+        RazborDefectoved,
+        NeedMoreInfoed,
+        NoVosproizvodimoed,
+        Returned,
+        Ispravlenied,
+        ProblemaReshenaed,
+        Closed,
+        PereOpened
+    }
+
+    public enum Action 
+    {
+        RazborDefectov,
+        NeedMoreInfo,
+        NoVosproizvodimo,
+        Return,
+        Ispravlenie,
+        ProblemaReshena,
+        Close,
+        PereOpene
+    }
+
+    public StateMachine<State, Action> _machine;
+
+    public Bug(State initialState = State.NewDefected) 
+    {
+        _machine = new StateMachine<State, Action>(initialState);
+
+        ConfigureMachine();
+    }
+
+    private void ConfigureMachine() 
+    {
+        _machine.Configure(State.NewDefected)
+            .Permit(Action.RazborDefectov, State.RazborDefectoved);
+
+        _machine.Configure(State.RazborDefectoved)
+            .Permit(Action.NeedMoreInfo, State.NeedMoreInfoed)
+            .Permit(Action.NoVosproizvodimo, State.NoVosproizvodimoed)
+            .Permit(Action.Ispravlenie, State.Ispravlenied);
+
+        _machine.Configure(State.NeedMoreInfoed)
+            .Permit(Action.RazborDefectov, State.RazborDefectoved)
+            .Permit(Action.Ispravlenie, State.Ispravlenied);
+
+        _machine.Configure(State.NoVosproizvodimoed)
+            .Permit(Action.Close, State.Closed)
+            .Permit(Action.Return, State.Returned);
+
+        _machine.Configure(State.Returned)
+            .Permit(Action.RazborDefectov, State.RazborDefectoved);
+
+        _machine.Configure(State.Ispravlenied)
+            .Permit(Action.ProblemaReshena, State.ProblemaReshenaed);
+
+        _machine.Configure(State.ProblemaReshenaed)
+            .Permit(Action.Close, State.Closed)
+            .Permit(Action.Return, State.Returned);
+
+        _machine.Configure(State.Closed)
+            .Permit(Action.PereOpene, State.PereOpened);
+
+        _machine.Configure(State.PereOpened)
+            .Permit(Action.RazborDefectov, State.RazborDefectoved);
+    }
+ 
+    public static void Main()
+    {
+        var bug = new Bug();
+        Console.WriteLine($"Initial State: {bug._machine.State}");
+
+        bug._machine.Fire(Action.RazborDefectov);
+        Console.WriteLine($"Current State: {bug._machine.State}");
+
+        bug._machine.Fire(Action.NeedMoreInfo);
+        Console.WriteLine($"Current State: {bug._machine.State}");
+
+        bug._machine.Fire(Action.Ispravlenie);
+        Console.WriteLine($"Current State: {bug._machine.State}");
+
+        bug._machine.Fire(Action.ProblemaReshena);
+        Console.WriteLine($"Current State: {bug._machine.State}");
+
+        bug._machine.Fire(Action.Close);
+        Console.WriteLine($"Current State: {bug._machine.State}");
+    }
+}
+}
---
+++ bugtests/UnitTest1.cs
@@ -0,0 +1,267 @@
+﻿// BugTests/UnitTest1.cs
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BugPro;
+
+namespace BugTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_InitialState_ShouldBeNewDefected()
+        {
+            // Arrange & Act
+            var bug = new Bug();
+
+            // Assert
+            Assert.AreEqual(Bug.State.NewDefected, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_NewDefected_To_RazborDefectoved_AfterRazborDefectov()
+        {
+            // Arrange
+            var bug = new Bug();
+
+            // Act
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+
+            // Assert
+            Assert.AreEqual(Bug.State.RazborDefectoved, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_RazborDefectoved_To_NeedMoreInfoed_AfterNeedMoreInfo()
+        {
+            // Arrange
+            var bug = new Bug();
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+
+            // Act
+            bug._machine.Fire(Bug.Action.NeedMoreInfo);
+
+            // Assert
+            Assert.AreEqual(Bug.State.NeedMoreInfoed, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_RazborDefectoved_To_NoVosproizvodimoed_AfterNoVosproizvodimo()
+        {
+            // Arrange
+            var bug = new Bug();
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+
+            // Act
+            bug._machine.Fire(Bug.Action.NoVosproizvodimo);
+
+            // Assert
+            Assert.AreEqual(Bug.State.NoVosproizvodimoed, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_RazborDefectoved_To_Ispravlenied_AfterIspravlenie()
+        {
+            // Arrange
+            var bug = new Bug();
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+
+            // Act
+            bug._machine.Fire(Bug.Action.Ispravlenie);
+
+            // Assert
+            Assert.AreEqual(Bug.State.Ispravlenied, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_NeedMoreInfoed_BackTo_RazborDefectoved_AfterRazborDefectov()
+        {
+            // Arrange
+            var bug = new Bug();
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+            bug._machine.Fire(Bug.Action.NeedMoreInfo);
+
+            // Act
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+
+            // Assert
+            Assert.AreEqual(Bug.State.RazborDefectoved, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_NeedMoreInfoed_To_Ispravlenied_AfterIspravlenie()
+        {
+            // Arrange
+            var bug = new Bug();
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+            bug._machine.Fire(Bug.Action.NeedMoreInfo);
+
+            // Act
+            bug._machine.Fire(Bug.Action.Ispravlenie);
+
+            // Assert
+            Assert.AreEqual(Bug.State.Ispravlenied, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_NoVosproizvodimoed_To_Closed_AfterClose()
+        {
+            // Arrange
+            var bug = new Bug();
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+            bug._machine.Fire(Bug.Action.NoVosproizvodimo);
+
+            // Act
+            bug._machine.Fire(Bug.Action.Close);
+
+            // Assert
+            Assert.AreEqual(Bug.State.Closed, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_NoVosproizvodimoed_To_Returned_AfterReturn()
+        {
+            // Arrange
+            var bug = new Bug();
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+            bug._machine.Fire(Bug.Action.NoVosproizvodimo);
+
+            // Act
+            bug._machine.Fire(Bug.Action.Return);
+
+            // Assert
+            Assert.AreEqual(Bug.State.Returned, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_Returned_To_RazborDefectoved_AfterRazborDefectov()
+        {
+            // Arrange
+            var bug = new Bug();
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+            bug._machine.Fire(Bug.Action.NoVosproizvodimo);
+            bug._machine.Fire(Bug.Action.Return);
+
+            // Act
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+
+            // Assert
+            Assert.AreEqual(Bug.State.RazborDefectoved, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_Ispravlenied_To_ProblemaReshenaed_AfterProblemaReshena()
+        {
+            // Arrange
+            var bug = new Bug();
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+            bug._machine.Fire(Bug.Action.Ispravlenie);
+
+            // Act
+            bug._machine.Fire(Bug.Action.ProblemaReshena);
+
+            // Assert
+            Assert.AreEqual(Bug.State.ProblemaReshenaed, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_ProblemaReshenaed_To_Closed_AfterClose()
+        {
+            // Arrange
+            var bug = new Bug();
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+            bug._machine.Fire(Bug.Action.Ispravlenie);
+            bug._machine.Fire(Bug.Action.ProblemaReshena);
+
+            // Act
+            bug._machine.Fire(Bug.Action.Close);
+
+            // Assert
+            Assert.AreEqual(Bug.State.Closed, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_ProblemaReshenaed_To_Returned_AfterReturn()
+        {
+            // Arrange
+            var bug = new Bug();
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+            bug._machine.Fire(Bug.Action.Ispravlenie);
+            bug._machine.Fire(Bug.Action.ProblemaReshena);
+
+            // Act
+            bug._machine.Fire(Bug.Action.Return);
+
+            // Assert
+            Assert.AreEqual(Bug.State.Returned, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_Closed_To_PereOpened_AfterPereOpene()
+        {
+            // Arrange
+            var bug = new Bug();
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+            bug._machine.Fire(Bug.Action.Ispravlenie);
+            bug._machine.Fire(Bug.Action.ProblemaReshena);
+            bug._machine.Fire(Bug.Action.Close);
+
+            // Act
+            bug._machine.Fire(Bug.Action.PereOpene);
+
+            // Assert
+            Assert.AreEqual(Bug.State.PereOpened, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_PereOpened_To_RazborDefectoved_AfterRazborDefectov()
+        {
+            // Arrange
+            var bug = new Bug();
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+            bug._machine.Fire(Bug.Action.Ispravlenie);
+            bug._machine.Fire(Bug.Action.ProblemaReshena);
+            bug._machine.Fire(Bug.Action.Close);
+            bug._machine.Fire(Bug.Action.PereOpene);
+
+            // Act
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+
+            // Assert
+            Assert.AreEqual(Bug.State.RazborDefectoved, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_FullWorkflow_NewDefectedToClosed_ShouldCompleteSuccessfully()
+        {
+            // Arrange
+            var bug = new Bug();
+
+            // Act
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+            bug._machine.Fire(Bug.Action.Ispravlenie);
+            bug._machine.Fire(Bug.Action.ProblemaReshena);
+            bug._machine.Fire(Bug.Action.Close);
+
+            // Assert
+            Assert.AreEqual(Bug.State.Closed, bug._machine.State);
+        }
+
+        [TestMethod]
+        public void Test_FullWorkflow_WithReopen_ShouldReturnToRazborDefectoved()
+        {
+            // Arrange
+            var bug = new Bug();
+
+            // Act
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+            bug._machine.Fire(Bug.Action.Ispravlenie);
+            bug._machine.Fire(Bug.Action.ProblemaReshena);
+            bug._machine.Fire(Bug.Action.Close);
+            bug._machine.Fire(Bug.Action.PereOpene);
+            bug._machine.Fire(Bug.Action.RazborDefectov);
+
+            // Assert
+            Assert.AreEqual(Bug.State.RazborDefectoved, bug._machine.State);
+        }
+    }
+}
